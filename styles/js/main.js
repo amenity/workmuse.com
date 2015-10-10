a32375261422e77fdd5ce7f8952c1d6b01a5f1ad
Amenity <amenity.applewhite@gmail.com>
2015-10-10 16:46:52 -0500

    simplifying

--- styles/js/main.js
+++ styles/js/main.js
@@ -79,32 +79,4 @@ jQuery(document).ready(function($){
 	$('.cd-nav-trigger').on('click', function(){
 		$('header').toggleClass('nav-is-visible');
 	});
-	//smooth scroll to the selected section
-	$('.cd-main-nav a[href^="#"]').on('click', function(event){
-        event.preventDefault();
-        $('header').removeClass('nav-is-visible');
-        var target= $(this.hash),
-        	topMargin = target.css('marginTop').replace('px', ''),
-        	hedearHeight = $('header').height();
-        $('body,html').animate({'scrollTop': parseInt(target.offset().top - hedearHeight - topMargin)}, 200); 
-    });
-    //update selected navigation element
-    $(window).on('scroll', function(){
-    	updateNavigation();
-    });
-
-    function updateNavigation() {
-		contentSections.each(function(){
-			var actual = $(this),
-				actualHeight = actual.height(),
-				topMargin = actual.css('marginTop').replace('px', ''),
-				actualAnchor = $('.cd-main-nav').find('a[href="#'+actual.attr('id')+'"]');
-			
-			if ( ( parseInt(actual.offset().top - $('.cd-main-nav').height() - topMargin )<= $(window).scrollTop() ) && ( parseInt(actual.offset().top +  actualHeight - topMargin )  > $(window).scrollTop() +1 ) ) {
-				actualAnchor.addClass('selected');
-			}else {
-				actualAnchor.removeClass('selected');
-			}
-		});
-	}
 });
