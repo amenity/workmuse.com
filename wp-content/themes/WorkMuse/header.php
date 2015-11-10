5babb3715db7e45d7271b1a0abfb56cde0ff8300
Amenity <amenity.applewhite@gmail.com>
2015-11-10 15:40:42 -0600

    preliminaty templates

--- wp-content/themes/WorkMuse/header.php
+++ wp-content/themes/WorkMuse/header.php
@@ -71,44 +71,15 @@
 		<div class="row">
 			<header class="twelve columns eld-header" role="banner">
 				<div class="row">
-					<div class="eight columns">
+					<div class="four columns">
 						<h1 id="site-title"><span><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span></h1>
 						<h4 id="site-description" class="subheader"><?php bloginfo( 'description' ); ?></h4>
 					</div>
-					<div class="four columns">
-						<?php wp_nav_menu( array(
-							'theme_location' => 'meta',
-							'container' => false,
-							'menu_class' => 'inline-list right',
-							'fallback_cb' => false
-						) ); ?>
-						<?php
-							/**
-							 * Search form
-							 *
-							 * If you need the search form just remove the //
-							 * infront of the following statement.
-							 *
-							 */
-							//get_search_form();
-						?>
+					<div class="eight columns">
+					<?php get_template_part( 'nav', 'top-bar' ); ?>
 					</div>
 				</div>
-				<?php
-					/**
-					 * Include the default navigation
-					 *
-					 * You could easily do something like:
-					 * if ( is_front_page() ) {
-					 * 	get_template_part( 'nav', 'front-page' ); // nav-front-page.php
-					 * } else {
-					 * 	get_template_part( 'nav' );	// nav.php
-					 * }
-					 */
-					if ( ! is_page_template( 'page-templates/off-canvas-page.php' ) ) {
-						get_template_part( 'nav', 'top-bar' );
-					}
-				?>
+
 
 				<?php
 					/**
