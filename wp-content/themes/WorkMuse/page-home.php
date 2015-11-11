99a08882282e81729ebc1e114dbc895628971491
Amenity <amenity.applewhite@gmail.com>
2015-11-11 13:50:35 -0600

    Kirby's home page template

--- wp-content/themes/WorkMuse/page-home.php
+++ wp-content/themes/WorkMuse/page-home.php
@@ -14,11 +14,15 @@
 get_header(); ?>
 
 	<!-- Row for main content area -->
-	<div class="somethingHuge">
-	<div class="row">
-	This should go all the way across
+	<?php $img = get_field('banner_image'); ?>
+	<div class="homeHero">
+		<img src="<?php echo $img['url']; ?>" alt="<?php echo $img['alt']; ?>" />
+		<div class="heroOverlay">
+			<?php the_field('banner_overlay'); ?>
+		</div>
 	</div>
-	</div>
+
+	
 
 	<div id="content" class="row">
 
@@ -27,18 +31,87 @@ get_header(); ?>
 
 				<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php get_template_part( 'content', 'page' ); ?>
+					<div class="pitch text-center">
+							<?php the_field('pitch'); ?>
+						</div>
 
-					<?php
-						/**
-						 * Seriously I never used comments on a page, what for?
-						 */
-						//comments_template( '', true );
-					?>
+
+
+						<?php
+						//Begin Image and Text Section
+						// check if the repeater field has rows of data
+						if( have_rows('image_text_section') ):
+						
+						 	// loop through the rows of data
+						    while ( have_rows('image_text_section') ) : the_row();
+						
+						        // display a sub field value
+						  		?>
+						  			<div class="six columns calloutImg">
+						  				<?php $imgtxt = get_sub_field('section_image');
+						  					$img = wp_get_attachment_image_src( $imgtxt, 'large' );
+						  					echo '<img src="'.$img[0] . '" />'; ?>
+
+						  			</div>
+
+						  			<div class="six columns calloutText">
+						  				<?php the_sub_field('section_text'); ?>
+						  			</div>
+						  			<div class="clearfix"></div>
+									<?php 
+						    endwhile;
+						
+						else :
+						
+						
+						endif;
+						//End Image and Text Section
+						?>
+
+						<?php
+						
+						// check if the repeater field has rows of data
+						if( have_rows('infographic') ):
+						
+						 	// loop through the rows of data
+						    while ( have_rows('infographic') ) : the_row();
+						
+						        // display a sub field value
+						        $img = get_sub_field('infographic_image');
+						        ?>
+						        <div class="infographicHome">
+						       		<img src="<?php echo $img['url']; ?>" alt="<?php echo $img['alt']; ?>" class="aligncenter"/>
+						        </div>
+						        <?php
+						
+						    endwhile;
+						
+						else :
+						
+						    // no rows found
+						
+						endif;
+						
+						?>
 
 				<?php endwhile; // end of the loop. ?>
 
+
+					<div class="homeCallOuts">
+							<div class="four columns text-center">
+								<a class="cta faq button" href="<?php echo get_the_permalink('18'); ?>">FAQ</a>
+							</div>
+							<div class="four columns text-center">
+								<a class="cta events button" href="/category/events/">Events</a>
+							</div>
+							<div class="four columns text-center">
+								<a class="cta newsletter button" href="#">Newsletter</a>							
+							</div>
+					</div>
 			</div>
+
+
+
 		</div><!-- /#main -->
 
 	</div><!-- End Content row -->
