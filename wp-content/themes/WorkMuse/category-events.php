c972b56f420a3a875fcf56a06b006c9f6874b5bf
Amenity <amenity.applewhite@gmail.com>
2015-12-08 17:27:36 -0600

    working tooward closing v1

--- wp-content/themes/WorkMuse/category-events.php
+++ wp-content/themes/WorkMuse/category-events.php
@@ -44,7 +44,14 @@ get_header(); ?>
 
           ?>
           <div class="three columns">
-          hexagon
+              <div class="hexy">
+                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 195.5 221.4" xml:space="preserve">
+                  <path class="roundedHex" d="M90.5,219.3l-83-48.1c-4.6-2.6-7.4-7.5-7.4-12.8L0,62.5c0-5.3,2.8-10.2,7.4-12.8L90.1,2  c4.6-2.6,10.2-2.6,14.8,0l83,48.1c4.6,2.6,7.4,7.5,7.4,12.8l0.2,95.9c0,5.3-2.8,10.2-7.4,12.8l-82.7,47.7  C100.8,222,95.1,222,90.5,219.3z" />
+                </svg>
+                <div class="date">
+                  <h3><?php the_field('event_date'); ?> </h3>
+                </div>  
+              </div>
           </div>
           <div class="nine columns">
           <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
